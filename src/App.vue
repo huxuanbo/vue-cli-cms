3a86a65a42419be44d2c84e3d9e39a76d1eecb34
hu hu <1037942434@qq.com>
2019-07-20 08:55:01 +0800

    7.20

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
+
+		<transition>
+		<router-view></router-view>
+		</transition>
+		
+
+        <nav class="mui-bar mui-bar-tab footer">
+			<router-link class="mui-tab-item" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge">10</span>
+				</span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    /* .footer {
+		background-color: #26A2FF!important;
+	} */
+	/* .footer .mui-tab-label,
+	.footer .mui-icon:before {
+		color: #fff;
+	} */
+	.container {
+		margin-top: 40px;
+		margin-bottom: 50px;
+		overflow-x: hidden;
+	}
+	.v-enter{
+		opacity: 0;
+		transform: translateX(100%);
+	}
+	.v-leave-to {
+		opacity: 0;
+		transform: translateX(-100%);
+	}
+	.v-enter-active,
+	.v-leave-active {
+		transition: all .4s ease;
+		position: absolute;
+		
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
 
-Vue.config.productionTip = false
+import Router from "vue-router"
+Vue.use(Router)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import VueResource from "vue-resource"
+Vue.use(VueResource)
+
+import MintUi from "mint-ui"
+Vue.use(MintUi)
+import "mint-ui/lib/style.css"
+
+
+import "./assets/mui/css/mui.css"
+import "./assets/mui/css/icons-extra.css"
+import "./assets/mui/fonts/mui-icons-extra.ttf"
+
+
+import router from "./router.js"
+
+var vm = new Vue({
+    el: "#app",
+    data: {
+
+    },
+    render: c => c(App),
+    router
+})
